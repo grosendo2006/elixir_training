5d5a66cf9d7e4b61dd55d44c394304b596ffa9f3
Gonzalo <gonzalo.rosendo@aero.tur.ar>
2020-05-04 11:00:13 -0300

    add test

--- phinx/test/phinx_web/controllers/user_controller_test.exs
+++ phinx/test/phinx_web/controllers/user_controller_test.exs
@@ -14,25 +14,34 @@ defmodule PhinxWeb.UserControllerTest do
 
   describe "index" do
     test "lists all users", %{conn: conn} do
-      conn = get(conn, Routes.user_path(conn, :index))
+      conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> get(Routes.user_path(conn, :index))
       assert json_response(conn, 200) == %{"data" => []}
     end
   end
 
   describe "create" do
     test "create user when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), user: @valid_attrs)
-      assert %{"id" => id} = json_response(conn, 201)
+      create_conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> post(Routes.user_path(conn, :create), user: @valid_attrs)
 
-      conn = get(conn, Routes.user_path(conn, :show, id))
+      assert %{"id" => id} = json_response(create_conn, 201)
+
+      show_conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> get( Routes.user_path(conn, :show, id))
       assert %{ "id" => id,
                 "name" => "Gonza",
                 "email" => "gon@za.com"
-              } = json_response(conn, 200)
+              } = json_response(show_conn, 200)
     end
 
     test "fallback when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
+      conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> post(Routes.user_path(conn, :create), user: @invalid_attrs)
       assert %{"errors" => %{"detail" => "Unprocessable Entity"}} = json_response(conn, 422)
     end
   end
@@ -41,22 +50,31 @@ defmodule PhinxWeb.UserControllerTest do
     setup [:create_user]
 
     test "update user when data is valid", %{conn: conn, user: user} do
-      conn = put(conn, Routes.user_path(conn, :update, user), user: @update_attrs)
+      update_conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> put(Routes.user_path(conn, :update, user), user: @update_attrs)
 
-      conn = get(conn, Routes.user_path(conn, :show, user.id))
+      show_conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> get(Routes.user_path(conn, :show, user.id))
       assert %{"id" => id,
                "name" => "Gonzalo",
                "email" => "gonza@lo.com"
-              } = json_response(conn, 200)
+              } = json_response(show_conn, 200)
     end
     test "get error when data is invalid", %{conn: conn, user: user} do
-      conn = put(conn, Routes.user_path(conn, :update, user), user: @invalid_attrs)
-      assert %{"errors" => %{"detail" => "Unprocessable Entity"}} = json_response(conn, 422)
-      conn = get(conn, Routes.user_path(conn, :show, user.id))
+      updaet_conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> put(Routes.user_path(conn, :update, user), user: @invalid_attrs)
+      assert %{"errors" => %{"detail" => "Unprocessable Entity"}} = json_response(updaet_conn, 422)
+
+      show_conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> get(Routes.user_path(conn, :show, user.id))
       assert %{ "id" => user.id,
                 "name" => user.name,
                 "email" => user.email
-              } == json_response(conn, 200)
+              } == json_response(show_conn, 200)
     end
   end
 
@@ -64,11 +82,15 @@ defmodule PhinxWeb.UserControllerTest do
     setup [:create_user]
 
     test "delete user", %{conn: conn, user: user} do
-      conn = delete(conn, Routes.user_path(conn, :delete, user))
-      assert response(conn, 204) == ""
+      delete_conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> delete(Routes.user_path(conn, :delete, user))
+      assert response(delete_conn, 204) == ""
 
-      conn = get(conn, Routes.user_path(conn, :show, user.id))
-      assert json_response(conn, 404) == %{"errors" => %{"detail" => "Not Found"}}
+      show_conn = conn
+      |> put_req_header("x-phinx-token", System.get_env("PHINX_TOKEN"))
+      |> get(Routes.user_path(conn, :show, user.id))
+      assert json_response(show_conn, 404) == %{"errors" => %{"detail" => "Not Found"}}
     end
   end
 
